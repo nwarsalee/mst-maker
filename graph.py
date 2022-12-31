b1b79febcd29013933d2d94735b9754230725fef
Nabeel Warsalee <nabeel.warsalee@gmail.com>
2022-12-30 21:08:34 -0500

    Get CSV import working for both edges and nodes

--- graph.py
+++ graph.py
@@ -10,6 +10,7 @@ from edge import Edge, Edges
 class Graph:
     def __init__(self, nodes_path, edges_path, import_csv=True):
         self.nodes = Nodes()
+        self.edges = Edges()
 
         if import_csv:
             # TODO: Perform functions that will read in CSV file and create nodes
@@ -25,9 +26,22 @@ class Graph:
             lines = csv.reader(file)
             # parse each line of file into a singular node
             for row in lines:
-                print(row)
+                node = Node(row[0], row[1])
+                self.nodes.add(node)
 
 
     def import_edges_csv(self, path):
-        # NOTE: Need to use nodes within nodes list (maybe need a find function)
-        pass
+        with open(path) as file:
+            lines = csv.reader(file)
+            # parse each line of file into a singular node
+            for row in lines:
+                # Find the nodes described in the entry
+                src = self.nodes.find(row[0])
+                dst = self.nodes.find(row[1])
+                weight = row[2]
+
+                # Create edge and add to collection of edges
+                edge = Edge(src, dst, int(weight))
+                self.edges.add(edge)
+        
+        print(self.edges)
